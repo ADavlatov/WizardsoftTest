ee260687a17c9a03c960694556cae665067e55d2
ADavlatov <a@davlatov.space>
2024-11-13 17:40:41 +0300

    Add context, controller and swagger in Program.cs

---
+++ WizardsoftTest.Server/Program.cs
@@ -0,0 +1,24 @@
+using AppContext = WizardsoftTest.Server.Database.AppContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppContext>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
