d619b0e4ea6ead3ee30f601d4f7b5a060e929e06
ADavlatov <a@davlatov.space>
2024-11-13 22:17:05 +0300

    Replace logic from controller to CategoriesService.cs

--- WizardsoftTest.Server/Controllers/CategoriesController.cs
+++ WizardsoftTest.Server/Controllers/CategoriesController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using WizardsoftTest.Server.Models;
 using WizardsoftTest.Server.Models.Requests;
+using WizardsoftTest.Server.Services;
 using AppContext = WizardsoftTest.Server.Database.AppContext;
 
 namespace WizardsoftTest.Server.Controllers;
@@ -10,11 +9,10 @@ namespace WizardsoftTest.Server.Controllers;
 [Route("api/v1/[controller]")]
 public class CategoriesController(AppContext db) : ControllerBase
 {
-    [HttpGet("{id}")]
-    public async Task<IActionResult> GetCategory(string id)
+    [HttpPost]
+    public async Task<IActionResult> AddCategory([FromBody] AddCategoryRequest request)
     {
-        var category = await db.Categories.Include(x
-            => x.Subcategories).FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+        var category = await new CategoriesService(db).AddCategory(request);
 
         if (category == null)
         {
@@ -23,75 +21,51 @@ public class CategoriesController(AppContext db) : ControllerBase
 
         return Ok(category);
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> GetCategories()
     {
-        var categories = await db.Categories.ToListAsync();
+        var category = await new CategoriesService(db).GetAllCategories();
 
-        return Ok(categories);
+        return Ok(category);
     }
 
-    [HttpPost]
-    public async Task<IActionResult> AddCategory([FromBody] AddCategoryRequest request)
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCategory(Guid id)
     {
-        Category category;
+        var category = await new CategoriesService(db).GetCategory(id);
 
-        if (!Guid.TryParse(request.ParentCategoryId, out _))
-        {
-            category = new Category
-                { Name = request.Name, ParentCategoryId = Guid.Empty };
-            await db.Categories.AddAsync(category);
-            await db.SaveChangesAsync();
-
-            return Ok(category);
-        }
-
-        var parentCategory =
-            await db.Categories.FirstOrDefaultAsync(x => x.Id == Guid.Parse(request.ParentCategoryId));
-        if (parentCategory == null)
+        if (category == null)
         {
             return NotFound();
         }
-
-        category = new Category
-            { Name = request.Name, ParentCategoryId = parentCategory.Id };
-        parentCategory.Subcategories.Add(category);
-        await db.SaveChangesAsync();
 
         return Ok(category);
     }
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryRequest request, string id)
+    public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryRequest request)
     {
-        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+        var category = await new CategoriesService(db).UpdateCategory(request);
 
         if (category == null)
         {
             return NotFound();
         }
-
-        category.Name = request.Name;
-        db.Categories.Update(category);
-        await db.SaveChangesAsync();
 
         return Ok(category);
     }
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteCategory(string id)
+    public async Task<IActionResult> DeleteCategory(Guid id)
     {
-        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+        var category = await new CategoriesService(db).DeleteCategory(id);
 
         if (category == null)
         {
             return NotFound();
         }
 
-        db.Categories.Remove(category);
-        await db.SaveChangesAsync();
-
-        return Ok();
+        return Ok(category);
     }
 }
---
+++ WizardsoftTest.Server/Services/CategoriesService.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WizardsoftTest.Server.Models;
+using WizardsoftTest.Server.Models.Requests;
+using AppContext = WizardsoftTest.Server.Database.AppContext;
+
+namespace WizardsoftTest.Server.Services;
+
+public class CategoriesService(AppContext db)
+{
+    public async Task<Category?> AddCategory(AddCategoryRequest request)
+    {
+        var category = new Category(request.Name, null);
+        
+        if (request.ParentCategoryId == null)
+        {
+            await db.Categories.AddAsync(category);
+            await db.SaveChangesAsync();
+
+            return category;
+        }
+
+        var parentCategory = await db.Categories.FirstOrDefaultAsync(x => x.Id == request.ParentCategoryId);
+        if (parentCategory == null)
+        {
+            return null;
+        }
+
+        category.ParentCategoryId = parentCategory.Id;
+        parentCategory.Subcategories.Add(category);
+        await db.SaveChangesAsync();
+
+        return category;
+    }
+
+    public async Task<List<Category>> GetAllCategories()
+    {
+        var categories = await db.Categories.ToListAsync();
+
+        return categories;
+    }
+
+    public async Task<Category?> GetCategory(Guid id)
+    {
+        var category = await db.Categories
+            .Include(x => x.Subcategories).FirstOrDefaultAsync(x => x.Id == id);
+
+        if (category == null)
+        {
+            return null;
+        }
+
+        return category;
+    }
+
+    public async Task<Category?> UpdateCategory(UpdateCategoryRequest request)
+    {
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+        if (category == null)
+        {
+            return null;
+        }
+
+        category.Name = request.Name;
+        db.Categories.Update(category);
+        await db.SaveChangesAsync();
+
+        return category;
+    }
+
+    public async Task<Category?> DeleteCategory(Guid id)
+    {
+        var category = await db.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (category == null)
+        {
+            return null;
+        }
+
+        db.Categories.Remove(category);
+        await db.SaveChangesAsync();
+
+        return category;
+    }
+}
