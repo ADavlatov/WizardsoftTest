13fbc0b1ea2b984a4830dcefd84e24c7b587bf4b
ADavlatov <a@davlatov.space>
2024-11-13 22:18:02 +0300

    Change to class with primary constructor

--- WizardsoftTest.Server/Models/Category.cs
+++ WizardsoftTest.Server/Models/Category.cs
@@ -1,9 +1,9 @@
 ﻿namespace WizardsoftTest.Server.Models;
 
-public record Category
+public class Category(string name, Guid? parentCategoryId)
 {
-    public Guid Id { get; init; }
-    public string Name { get; set; }
-    public List<Category> Subcategories { get; init; } = new();
-    public Guid ParentCategoryId { get; init; }
+    public Guid Id { get; }
+    public string Name { get; set; } = name;
+    public ICollection<Category> Subcategories { get; init; } = new List<Category>();
+    public Guid? ParentCategoryId { get; set; } = parentCategoryId;
 }
