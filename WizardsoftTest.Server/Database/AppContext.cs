442a3072fea4733cee0b8495ab13a578783c6b27
ADavlatov <a@davlatov.space>
2024-11-13 17:40:24 +0300

    Add ef core context and category model

---
+++ WizardsoftTest.Server/Database/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WizardsoftTest.Server.Models;
+
+namespace WizardsoftTest.Server.Database;
+
+public sealed class AppContext : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+
+    public AppContext()
+    {
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Database/app.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(x => x.Id);
+            entity.HasMany(x => x.Subcategories);
+        });
+    }
+}
---
+++ WizardsoftTest.Server/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WizardsoftTest.Server.Models;
+
+public record Category
+{
+    public Guid Id { get; init; }
+    public string Name { get; set; }
+    public List<Category> Subcategories { get; init; } = new();
+    public Guid ParentCategoryId { get; init; }
+}
