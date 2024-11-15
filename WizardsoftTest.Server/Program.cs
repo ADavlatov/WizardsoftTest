7f7399be880ca8e1516035fee30bf81a0f9f2fb4
ADavlatov <a@davlatov.space>
2024-11-15 23:19:58 +0300

    Add repository

--- WizardsoftTest.Server/Program.cs
+++ WizardsoftTest.Server/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using WizardsoftTest.Server.Interfaces;
+using WizardsoftTest.Server.Repositories;
 using AppContext = WizardsoftTest.Server.Database.AppContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppContext>(options => options.UseSqlite("Data Source=Database/app.db"));
+builder.Services.AddTransient<ICategoriesRepository, CategoriesRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
