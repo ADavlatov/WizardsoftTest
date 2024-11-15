dfb62035e45ea22ef5544a1c6e81f55f404d86b7
ADavlatov <a@davlatov.space>
2024-11-15 23:18:24 +0300

    Add repository interface

---
+++ WizardsoftTest.Server/Interfaces/ICategoriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using WizardsoftTest.Server.Entities;
+
+namespace WizardsoftTest.Server.Interfaces;
+
+public interface ICategoriesRepository
+{
+    Task<List<Category>> GetAllCategories();
+    Task<Category> GetCategory(Guid id);
+    Task<Category> AddCategory(Category category, Category? parentCategory);
+    Task<Category> UpdateCategory(Category category);
+    Task<Category> DeleteCategory(Guid id);
+}
